4202bb1578890a00dc5c32df9d8ed1de66400713
cvsintellect <admin@cvsintellect.com>
2014-04-19 08:18:58 +0530

    install texlive-full through script

--- data/setup.sh
+++ data/setup.sh
@@ -5,7 +5,9 @@ apt-get install git-core
 apt-get install nodejs-legacy
 apt-get install npm
 apt-get install mysql-server
-apt-get install texlive-full
+
+# apt get install texlive-full
+wget https://github.com/scottkosty/install-tl-ubuntu/raw/master/install-tl-ubuntu && chmod +x ./install-tl-ubuntu && ./install-tl-ubuntu
 
 npm install -g grunt grunt-cli
 
@@ -14,7 +16,16 @@ cd clsi-sharelatex
 npm install
 grunt install
 
+# reboot
+
 # mysql -u root -p
 # CREATE DATABASE clsi;
+# SHOW DATABASES;
+# exit
 
 # change config/settings.defaults.coffee [username (root), password (root password), dialect (mysql), port (80), host (public domain), url (http://public domain)]
+
+# grunt run > out.log 2> error.log &
+# disown
+
+# tlmgr update --self --all --reinstall-forcibly-removed
